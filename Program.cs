e75111ff97d3f351493f8ed49d76dd1af9472412
JidapaBubu <jidapa4499@gmail.com>
2022-11-24 23:30:20 +0700

    พยายามที่สุดแล้วจริงๆ ค่ะ

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program {
+     
+     static void Main(string[] args)
+    {  
+        CircularLinkedList<char> = new CircularLinkedList<char>();
+
+        while(true)
+        {
+            bool canAdd = true;
+            char n = char.Parse(Console.ReadLine());
+            if(n!='J'&& n!='G' && n!='O' && n!='R'){break;}
+
+
+        }
+}
